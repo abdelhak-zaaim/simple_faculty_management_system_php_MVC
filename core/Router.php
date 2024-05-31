39b5a510c950205144f66be6ba36c43c37de7ba8
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-05-31 18:46:09 +0100

    implements the Router functionality

--- core/Router.php
+++ core/Router.php
@@ -1,5 +1,25 @@
 <?php
 class Router {
     public function route($url) {
+        // Split the URL into parts
+        $urlArray = explode('/', $url);
+
+        // The first part is the controller
+        $controller = ucfirst($urlArray[1]);
+
+        // The second part is the method
+        $method = $urlArray[2];
+
+        // The third part is the ID
+        $id = $urlArray[3];
+
+        // Include the controller file
+        include 'controllers/' . $controller . '.php';
+
+        // Create a new instance of the controller
+        $controller = new $controller();
+
+        // Call the method
+        $controller->$method($id);
     }
 }
