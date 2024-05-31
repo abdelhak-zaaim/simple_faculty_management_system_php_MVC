ce961f711ca68571abaad4358c9eb990709d67df
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-05-31 17:19:32 +0100

    add authentication conception

---
+++ core/Auth.php
@@ -0,0 +1,21 @@
+<?php
+class Auth {
+    public function register($username, $password) {
+
+    }
+
+    public function login($username, $password) {
+
+    }
+
+    public function logout() {
+
+    }
+
+    public function check() {
+    }
+
+    public function user() {
+
+    }
+}
